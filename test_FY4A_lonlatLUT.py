28c64991919b49c6944cecdc11822458d779cf30
BeichenZhang <zbc555day@163.com>
2023-05-05 19:06:47 +0900

    读取投影转换LUT

---
+++ test_FY4A_lonlatLUT.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+LUT_path = '/disk2/workspace/20230505/FullMask_Grid_1000/FullMask_Grid_1000.raw'
+lon_npy_path = '/disk2/workspace/20230505/FullMask_Grid_1000/FullMask_Grid_1000_lon.npy'
+lat_npy_path = '/disk2/workspace/20230505/FullMask_Grid_1000/FullMask_Grid_1000_lat.npy'
+
+if __name__ == "__main__":
+    # 打开文件并读取所有数据
+    with open(LUT_path, 'rb') as f:
+        data = f.read()
+
+    # 将数据解析为一个 Numpy 数组
+    # '<' 表示使用小端字节序，'d' 表示使用 double 类型
+    arr = np.frombuffer(data, dtype='<d')
+
+    # 将数据按照经度和纬度分开，并且每个网格用一个元组表示
+    grid_size = 16  # 每个网格占用 16 字节
+    grid_count = len(arr) // (grid_size // 8)   # 计算网格的数量
+    lon_arr = arr[0:grid_count*2:2]
+    lat_arr = arr[1:grid_count*2:2]
+
+    # grid_arr = np.array(list(zip(lon_arr, lat_arr)))
+    # print(grid_arr.shape)
+    lon_2d = lon_arr.reshape((10992, 10992))
+    lat_2d = lat_arr.reshape((10992, 10992))
+    np.save(lon_npy_path, lon_2d)
+    np.save(lat_npy_path, lat_2d)
+
