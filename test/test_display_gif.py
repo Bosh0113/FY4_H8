24ca1fc1dfd4488d921f7e559aa21a29849601b0
BeichenZhang <zbc555day@163.com>
2023-05-10 15:28:34 +0900

    地理配准动图

--- test/test_display_gif.py
+++ test/test_display_gif.py
@@ -10,9 +10,11 @@ def create_gif(image_list, gif_name, duration=1):
 
 
 if __name__ == "__main__":
-    gif_imgs = [
-        r'E:\PhD_Workspace\FY4_H8_WS\20230509\figure\FY4_FYCenter_202106190300_NIR_s1.png',
-        r'E:\PhD_Workspace\FY4_H8_WS\20230509\figure\FY4_GeoNEX_202106190300_NIR_s1.png'
-    ]
-    gif_path = r'E:\PhD_Workspace\FY4_H8_WS\20230509\figure\FY4A_GeoNEX-FYCenter_s1.gif'
+    figure_folder = r'E:\PhD_Workspace\FY4_H8_WS\20230510'
+    time_list = ['20210619010000', '20210619020000', '20210619024500', '20210619030000', '20210619031500', '20210619040000', '20210619050000', '20210619054500', '20210619060000', '20210619061500', '20210619070000']
+    gif_imgs = []
+    for time_str in time_list:
+        figure_path = figure_folder + '/geolocation_FY4A_AGRI_' + time_str + '.png'
+        gif_imgs.append(figure_path)
+    gif_path = figure_folder + '/FY4A_GeoNEX_20210619.gif'
     create_gif(gif_imgs, gif_path)
